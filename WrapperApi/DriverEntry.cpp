adb0f2906552a13c54a5f3b328ba3bff6c06edd0
LazyAhora <84189666+LazyAhora@users.noreply.github.com>
2021-10-09 13:03:10 +0300

    add sample

--- WrapperApi/DriverEntry.cpp
+++ WrapperApi/DriverEntry.cpp
@@ -13,9 +13,23 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING pRegist
 {
 	UNREFERENCED_PARAMETER(pRegistryPath);
  
-	Log("Windows number ->\t %u\n", ApiWrapper::GetWindowsNumber());
+	auto dayWrapper = ApiWrapper::InitUnicodeString(L"Test day");
+	UNICODE_STRING dayGood;
+	RtlInitUnicodeString(&dayGood, L"Test day");
+	Log("buffer no wrapper ->\t 0x%p\n", dayGood.Buffer);
+	Log("buffer wrapper ->\t 0x%p\n", dayWrapper.Buffer);
 
-	Log("Windows NtOskrlnBase ->\t 0x%p\n", GetKernelBase());
+	Log("lenght no wrapper ->\t 0x%p\n", dayGood.Length);
+	Log("lenght wrapper ->\t 0x%p\n", dayWrapper.Length);
+ 
+	Log("Max lenght no wrapper ->\t 0x%p\n", dayGood.MaximumLength);
+	Log("Max lenght wrapper ->\t 0x%p\n", dayWrapper.MaximumLength);
+	
+	Log("Windows number ->\t 0x%u", ApiWrapper::GetWindowsNumber());
+
+	Log("Windows build number ->\t 0x%u", ApiWrapper::GetNumberBuild());
+
+	ApiWrapper::KeMessageBox(L"Test", L"Good day", MB_OK);
 
    
 	return STATUS_SUCCESS;
