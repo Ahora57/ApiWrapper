d1cb8818e2464973e6cec84fbc6a86947a3c12e1
LazyAhora <84189666+LazyAhora@users.noreply.github.com>
2021-10-09 12:56:14 +0300

    Update Struct.h

--- WrapperApi/Struct.h
+++ WrapperApi/Struct.h
@@ -6,9 +6,26 @@
 #include <minwindef.h>
 #include <intrin.h>
 
+typedef enum _uTypeBoxReason 
+{
+	 MB_OK,
+	 MB_OKCANCEL,
+	 MB_ABORTRETRYIGNORE,
+	 MB_YESNOCANCEL,
+	 MB_YESNO,
+	 MB_RETRYCANCEL,
+	 MB_HELP = 0x00004000L
+}uTypeBoxReason;
 
 
 #define Log(x,...)  DbgPrintEx(0, 0, "[ApiWrapper] " x, __VA_ARGS__)
  
  
- 
+ EXTERN_C NTSTATUS NTAPI ExRaiseHardError
+(	NTSTATUS ErrorStatus,
+	ULONG NumberOfParameters,
+	ULONG UnicodeStringParameterMask, 
+	PULONG_PTR Parameters,
+	ULONG ValidResponseOptions,
+	PULONG Response
+);
